765613f7ae558c333ff54a400a59fea964790a66
lorenzo <lorenzo.santi359@gmail.com>
2026-08-11 20:04:42 +0200

    aggiunta BBL

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/EWing/LukeSkywalker.cs
@@ -0,0 +1,32 @@
+using Abilities.SecondEdition;
+using Upgrade;
+using System.Collections.Generic;
+using Content;
+
+namespace Ship
+{
+    namespace SecondEdition.EWing
+    {
+        public class LukeSkywalker : EWing
+        {
+            public LukeSkywalker() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Luke Skywalker",
+                    6,
+                    90,
+                    isLimited: true,
+                    abilityType: typeof(LukeSkywalkerAbility),
+                    tags: new List<Tags>
+                    {
+                        Tags.LightSide
+                    },
+                    force: 3,
+                    extraUpgradeIcon: UpgradeType.ForcePower
+                );
+                PilotNameCanonical = "lukeskywalker-ewing";
+                ModelInfo.SkinName = "Luke Skywalker";
+            }
+        }
+    }
+}
