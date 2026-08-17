0f20ff9909467831e3c6e2e479a9ee0142a87f9a
lorenzo <lorenzo.santi359@gmail.com>
2026-08-17 22:39:02 +0200

    17 ago: aggiunta piloti scrambler e dis067

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/DroidTriFighter/DIS067.cs
@@ -0,0 +1,55 @@
+﻿using Content;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using Tokens;
+using Upgrade;
+using ActionsList;
+using BoardTools;
+using Movement;
+
+namespace Ship.SecondEdition.DroidTriFighter
+{
+    public class DIS067 : DroidTriFighter
+    {
+        public DIS067()
+        {
+            PilotInfo = new PilotCardInfo(
+                "DIS-067",
+                4,
+                38,
+                limited: 2,
+                extraUpgradeIcon: UpgradeType.Talent,
+                abilityType: typeof(Abilities.SecondEdition.DIS067Ability),
+                tags: new List<Tags>
+                {
+                    Tags.Droid
+                }
+            );
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class DIS067Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGetAvailableBarrelRollTemplates += ChangeBarrelRollTemplates;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGetAvailableBarrelRollTemplates -= ChangeBarrelRollTemplates;
+        }
+
+        private void ChangeBarrelRollTemplates(List<ManeuverTemplate> availableTemplates, GenericAction action)
+        {
+            availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.Turn, ManeuverDirection.Left, ManeuverSpeed.Speed1));
+            availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.Turn, ManeuverDirection.Right, ManeuverSpeed.Speed1));
+            availableTemplates.RemoveAll(n => n.Name == "Straight 1");
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/NimbusClassVWing/Scrambler.cs
@@ -0,0 +1,84 @@
+﻿using BoardTools;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using Upgrade;
+using System.Linq;
+
+namespace Ship
+{
+    namespace SecondEdition.NimbusClassVWing
+    {
+        public class Scrambler : NimbusClassVWing
+        {
+            public Scrambler() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Scrambler\"",
+                    3,
+                    30,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.ScramblerAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class ScramblerAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            AddDiceModification(
+                HostShip.PilotInfo.PilotName,
+                CanBeUsed,
+                GetAiPriority,
+                DiceModificationType.Reroll,
+                2,
+                isGlobal: true,
+                payAbilityCost: SpendTargetLockOnAttacker
+            );
+        }
+
+        private bool CanBeUsed()
+        {
+            if (!Tools.IsFriendly(Combat.Defender, HostShip)) return false;
+            DistanceInfo distInfo = new DistanceInfo(HostShip, Combat.Attacker);
+            if (distInfo.Range < 1 || distInfo.Range > 3) return false;
+            return (Combat.AttackStep == CombatStep.Defence && ActionsHolder.HasTargetLockOn(HostShip, Combat.Attacker));
+        }
+
+        private int GetAiPriority()
+        {
+            return 85;
+        }
+
+        private void SpendTargetLockOnAttacker(Action<bool> callback)
+        {
+            if (ActionsHolder.HasTargetLockOn(HostShip, Combat.Attacker))
+            {
+                SpendTargetLock(delegate { callback(true); });
+            }
+            else
+            {
+                Messages.ShowError("Error: The attacker has no Target Lock to spend");
+                callback(false);
+            }
+        }
+
+        private void SpendTargetLock(Action callBack)
+        {
+            List<char> letters = ActionsHolder.GetTargetLocksLetterPairs(HostShip, Combat.Attacker);
+            HostShip.Tokens.SpendToken(typeof(Tokens.BlueTargetLockToken), callBack, letters.First());
+        }
+
+        public override void DeactivateAbility()
+        {
+            RemoveDiceModification();
+        }
+    }
+}
